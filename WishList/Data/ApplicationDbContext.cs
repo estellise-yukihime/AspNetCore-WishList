95f831746ce3845794f9fa3bfd6637d952699038
estellise-yukihime <shiranoyukihime@gmail.com>
2023-03-24 21:18:39 +0800

    update

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items
+        {
+            get;
+            set;
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
